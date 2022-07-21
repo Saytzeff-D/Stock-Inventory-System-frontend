8b83f29c16da6e4ea93f6c0f9a7c32efdb5fe5ee
Saytzeff-D <davidatanda2020@gmail.com>
2022-07-21 13:49:25 +0100

    made modifications to route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { LayoutBackgroundComponent } from './layout-background/layout-background.component';
 import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: '', redirectTo: 'layout/login', pathMatch: 'full'},
+  {path: 'layout', component: LayoutBackgroundComponent, children: [
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent}
+  ]},
+  {path: 'admin', component: AdminComponent},
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
